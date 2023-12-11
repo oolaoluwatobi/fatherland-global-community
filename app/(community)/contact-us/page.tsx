7e2ca97c1fde496af974148bb98bc4f58f1d8e7c
that9jaboy3 <olatunjia889@gmail.com>
2023-12-11 08:48:56 +0100

    contact

---
+++ app/(community)/contact-us/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Contactus from './components/Contactus'
+
+export default function pageContact() {
+  return (
+    <div className='mx-auto max-w-[90rem] bg-[#fcfdff]'>
+        <Contactus />
+    </div>
+  )
+}
