9a486e4befc1f8e1034787daa6989edc148c41de
that9jaboy3 <olatunjia889@gmail.com>
2023-12-27 22:03:33 +0100

    africantaste

--- app/(community)/find-a-restaurant/components/Find.tsx
+++ app/(community)/find-a-restaurant/components/Find.tsx
@@ -1,18 +1,28 @@
-'use client'
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
-import React from 'react'
-
+"use client";
+import React from "react";
 
 export default function Find() {
   return (
-    <div className='max-w-[90rem] mx-auto'>
-        <div className='max-w-[90rem] bg-[#2B3449] pt-[6.563rem] lg:px-[6.188rem]'>
-            <p className={`text-white font-bold text-[40px] break-words leading-[56px] pb-[1.438rem] mx-auto container`}>Find a Restaurant</p>
-            <div className="relative lg:pl[2rem] mx-auto container  pb-[4.438rem]">
-              <input type="text" placeholder='Search restaurant name' className=' mb-5 w-full mr-[1.375rem] lg:w-[35.063rem] py-[1.625rem] focus:outline-none bg-[#5c6271] rounded-[8px] text-white text-base font-medium break-words pl-[1.75rem]'/>
-              <input type="text" placeholder='Location' className='w-full lg:w-[35.063rem] py-[1.625rem] focus:outline-none bg-[#5c6271] rounded-[8px] text-white text-base font-medium break-words pl-[1.75rem]'/>
-            </div>
+    <div className="mx-auto bg-[#2B3449] lg:pt-[6.563rem] pt-12 lg:px-[6.188rem]">
+      <div className="max-w-7xl mx-auto">
+        <p
+          className={`text-white font-bold lg:text-[40px] text-3xl break-words leading-[56px] pb-[1.438rem] mx-auto container`}
+        >
+          Find a Restaurant
+        </p>
+        <div className="relative lg:pl[2rem] mx-auto container pb-[4.438rem]">
+          <input
+            type="text"
+            placeholder="Search restaurant name"
+            className=" mb-5 w-full mr-[1.375rem] lg:w-[35.063rem] lg:py-[1.625rem] py-3 focus:outline-none bg-[#5c6271] rounded-[8px] text-white text-base font-medium break-words pl-[1.75rem]"
+          />
+          <input
+            type="text"
+            placeholder="Location"
+            className="w-full lg:w-[35.063rem] lg:py-[1.625rem] py-3 focus:outline-none bg-[#5c6271] rounded-[8px] text-white text-base font-medium break-words pl-[1.75rem]"
+          />
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- app/(community)/find-a-restaurant/components/Foodsection.tsx
+++ app/(community)/find-a-restaurant/components/Foodsection.tsx
@@ -8,27 +8,28 @@ import { useRouter } from "next/navigation";
 export default function Foodsection() {
   const router = useRouter();
   return (
-    <div className="mx-auto bg-green950 ">
-      <main className="mx-auto bg-red500 lg:flex flex-wrap justify-around max-w-7xl pt-[8.313rem]">
+    <div className="mx-auto bg-green950 container max-w-7xl">
+      <main className="mx-auto bg-red500 lg:grid grid-cols-3 gap-[2.125rem] lg:pt-[8.313rem] pt-12 lg:mb-32 mb-12">
         {foodSection.map((food, i) => (
           <div
             key={i}
-            className={`max-w-[24.25rem] container mb-[5.063rem shadow-lg shadow-blue-100 mb-[7.813rem]`}
+            className={`max-w-[24.25rem] shadow-lg shadow-blue-100 lg:mb-[5.063rem] mb-8 rounded-[8px]`}
           >
             <Image src={food.image} alt="" className="mb-[1.688rem]" />
-            <p className="max-w-[6.813rem] bg-[#e7f6f8] rounded-[16.50px] border-none text-[#0BA1BA] text-sm font-normal leading-5 break-words pl-[1.063rem] py-[0.25rem] mb-[1.438rem]">
+            <p className="max-w-[6.813rem] mx-[1.063rem] bg-[#e7f6f8] rounded-[16.50px] border-none text-[#0BA1BA] text-sm 
+            font-normal leading-5 break-words pl-[1.063rem] py-[0.25rem] mb-[1.438rem]">
               {food.location}
             </p>
-            <p className="text-[#14202d] font-normal text-xl leading-4 break-words mb-[1.063rem]">
+            <p className="text-[#14202d] font-normal lg:text-xl text-sm leading-4 break-words mb-[1.063rem] px-[1.063rem]">
               {food.label}
             </p>
-            <div className="flex justify-between items-center mb-[1.188rem]">
-              <p className="text-[#666666] text-sm font-normal leading-5 break-words">
+            <div className="flex justify-between items-center pb-[1.188rem] px-[1.063rem]">
+              <p className="text-[#666666] lg:text-sm text-xs font-normal leading-5 break-words">
                 {food.text}
               </p>
 
               <Button
-                className="text-[#0BA0BD] bg-white hover:bg-white 
+                className="text-[#0BA0BD] bg-white hover:bg-white mb8
                             text-sm font-semibold break-words border border-[#0BA0BD]"
                 onClick={() => router.push(`/find-a-restaurant/${food.id}`)}
               >
