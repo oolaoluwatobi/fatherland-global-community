3b30b2766c0be46853d3f34a9855dabfd6b191c1
that9jaboy3 <olatunjia889@gmail.com>
2023-12-29 11:49:56 +0100

    tickets

--- app/(community)/tickets-and-events/components/BrowseCategories.tsx
+++ app/(community)/tickets-and-events/components/BrowseCategories.tsx
@@ -3,7 +3,6 @@ import { CATEGORIES } from "@/lib/data";
 // import React, { ReactNode } from "react";
 
 export default function BrowseCategories() {
-
   // const renderCategories = CATEGORIES.map((cat) => {
   //   <div className="grid grid-cols-3 mx-auto gap-8">
   //     {CATEGORIES.map((cat, index) => (
@@ -24,13 +23,22 @@ export default function BrowseCategories() {
 
   return (
     <Container>
-      <div className="mt-10 px-8">
-        <h5 className="text-4xl font-[700]  py-5 ">Browse Categories</h5>
-        <div className="fle flex-wra grid grid-cols-3 mx-auto place-items-center gap-8">
+      <div className="mt10 px-8">
+        <h5 className="lg:text-4xl text-2xl font-[700]  py-5 ">
+          Browse Categories
+        </h5>
+        <div className=" grid lg:grid-cols-3 mx-auto place-items-center gap-8">
           {CATEGORIES.map((cat) => (
-            <div key={cat.name} className="flex px-8 py-5 w96 w-full mx-auto rounded bg-secondary-foreground m4 border border-[#FD6906]  cursor-pointer">
-              <div className="bg-red-20 my-auto w10 mr-8 text-primary">{cat.icon}</div>
-              <p className="my-auto   w-ful text-center bg-red-20 font-[700] ">{cat.name}</p>
+            <div
+              key={cat.name}
+              className="flex px-8 py-5 w96 w-full mx-auto rounded bg-secondary-foreground m4 border border-[#FD6906]  cursor-pointer"
+            >
+              <div className="bg-red-20 my-auto w10 mr-8 text-primary">
+                {cat.icon}
+              </div>
+              <p className="my-auto w-ful text-center bg-red-20 font-[500] lg:text-xl text-sm ">
+                {cat.name}
+              </p>
             </div>
           ))}
         </div>
