4d64d5b185f41cd25d96c85f9272a6341bd43a95
Temitopeoyin <temitopeoyin8@gmail.com>
2024-01-29 12:33:09 +0100

    authentication

--- app/(authpages)/becomeamember/components/Join.tsx
+++ app/(authpages)/becomeamember/components/Join.tsx
@@ -1,12 +1,80 @@
 "use client";
 
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import Image from "next/image";
 import no1 from "@/public/no1.png";
 import card1 from "@/public/card1.png";
 import card2 from "@/public/card2.png";
+import Link from "next/link";
+import axios from "axios";
+import { Loader } from "lucide-react";
+import { redirect } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function Join() {
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_number, setPhoneNumber] = useState("");
+  const [date_of_birth, setDob] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [isPending, setIsPending] = useState(false);
+  const [errorMessage, setErrorMesage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // console.log(
+    //   last_name,
+    //   email,
+    //   password,
+    //   date_of_birth,
+    //   country,
+    //   state,
+    //   phone_number
+    // );
+    setIsPending(true);
+    // toast.loading('Signing up...')
+
+    try {
+      const data = {
+        first_name,
+        last_name,
+        email,
+        password,
+        date_of_birth,
+        country,
+        state,
+        phone_number,
+      };
+      console.log("[USERDATA]", data);
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/register`,
+        data
+      );
+      // addUserData(data);
+      // replace("/checkemail");
+      // const res: any = formAction(formData);
+      console.log(data, res, "[SIGNUP_INIT_RES]");
+      toast.success("Signed up successfully")
+      if(res.data.status === 200) {
+        redirect('/payment-details')
+      }
+      
+    } catch (error: any) {
+      toast.error("Something went wrong, Signed up failed")
+      console.log("Error", error);
+      if (error?.response?.data?.message)
+        setErrorMesage(error?.response?.data?.message);
+    } finally {
+      setIsPending(false);
+      // toast.loading
+    }
+  };
+
   return (
     <div className="mx-auto lg:pt-[5.563rem] pt-12 max-w-7xl">
       <div className="bg-red300 mx-auto lg:mb-[3.813rem] mb-12">
@@ -27,85 +95,138 @@ export default function Join() {
             </p>
           </div>
 
-          <form action="" className="mxauto px-[2.25rem] pb-[2.914rem]">
+          <form
+            onSubmit={(e) => handleSubmit(e)}
+            action=""
+            className="mxauto px-[2.25rem] pb-[2.914rem]">
             <div className="lg:flex justify-around lg:pb-[2.688rem] pb-4 gap-4 mx-auto">
               <div>
-                <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+                <label
+                  htmlFor=""
+                  className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
                   First Name
                 </label>{" "}
                 <br />
                 <input
-                  type="first_name"
-                  className="w-full lg:w-[20rem] lg:h-[65px] h-12 mb-4 bg-[#f4f4f4  bg-[#f4f4f4] rounded-[6px] lg:mb-0 placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
+                  type="text"
+                  id="first_name"
+                  name="first_name"
+                  value={first_name}
+                  required
+                  onChange={(e) => setFirstName(e.target.value)}
+                  className="w-full lg:w-[20rem] lg:h-[65px] h-12 mb-4 bg-[#f4f4f4  bg-[#f4f4f4] rounded-[6px] lg:mb-0 placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem ps-4"
                 />
               </div>
               <div>
-                <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+                <label
+                  htmlFor=""
+                  className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
                   Last Name
                 </label>{" "}
                 <br />
                 <input
-                  type="last_name"
+                  type="text"
+                  id="last_name"
+                  name="last_name"
+                  value={last_name}
+                  required
+                  onChange={(e) => setLastName(e.target.value)}
                   className=" w-full lg:w-[20rem] lg:h-[65px] h-12 bg-[#f4f4f4  bg-[#f4f4f4] rounded-[6px] placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
                 />
               </div>
             </div>
-            <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words ">
+            <label
+              htmlFor=""
+              className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words ">
               Email Address
             </label>{" "}
             <br />
             <input
               type="email"
+              id="email"
+              name="email"
+              value={email}
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="example@gmail.com"
               className="bg-[#f4f4f4] w-full lg:h-[65px] h-12 mb-4 lg:mb-[2.688rem] rounded-[6px] placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
             />
-            <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+            <label
+              htmlFor=""
+              className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
               Phone Number
             </label>{" "}
             <br />
             <input
-              type="phome_number"
+              type="number"
+              id="phone_number"
+              name="phone_number"
+              value={phone_number}
+              onChange={(e) => setPhoneNumber(e.target.value)}
               className="bg-[#f4f4f4] w-full lg:h-[65px] h-12 mb-4 lg:mb-[2.688rem] rounded-[6px] placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
             />
-            <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+            <label
+              htmlFor=""
+              className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
               Date of Birth
             </label>{" "}
             <br />
             <input
-              type="dob"
+              type="date"
+              id="date_of_birth"
+              name="date_of_birth"
+              value={date_of_birth}
+              onChange={(e) => setDob(e.target.value)}
+              required
               className="bg-[#f4f4f4] w-full lg:h-[65px] h-12 mb-4 lg:mb-[2.688rem] rounded-[6px] placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
             />
             <div className="lg:flex justify-between mx-auto gap-4 pb-[2.688rem]">
               <div>
-                <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+                <label
+                  htmlFor=""
+                  className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
                   Country
                 </label>{" "}
                 <br />
                 <input
-                  type="country"
+                  type="text"
+                  id="country"
+                  name="country"
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
                   className="w-full lg:w-[20rem] lg:h-[65px] h-12 bg-[#f4f4f4  bg-[#f4f4f4] rounded-[6px]  mb-4 lg:mb-0 placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
                 />
               </div>
               <div>
-                <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+                <label
+                  htmlFor=""
+                  className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
                   CIty/State/Province
                 </label>{" "}
                 <br />
                 <input
-                  type="state"
+                  type="text"
+                  id="state"
+                  name="state"
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
                   className=" w-full lg:w-[20rem] lg:h-[65px] h-12 bg-[#f4f4f4 bg-[#f4f4f4] rounded-[6px] placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
                 />
               </div>
             </div>
-            <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+            {/* <label
+              htmlFor=""
+              className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
               Select preferred community hub
             </label>{" "}
             <br />
             <select
               name=""
               id=""
-              className="w-full bg-[#f4f4f4] lg:h-[65px] h-12 mb-4 lg:mb-[2.688rem] rounded-[6px]"
-            >
-              <option value="" className="px-[2rem text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+              className="w-full bg-[#f4f4f4] lg:h-[65px] h-12 mb-4 lg:mb-[2.688rem] rounded-[6px]">
+              <option
+                value=""
+                className="px-[2rem text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
                 Fatherland Lagos
               </option>
               <option value="" className="px-[0.999rem">
@@ -120,26 +241,43 @@ export default function Join() {
               <option value="" className="px-[0.999rem">
                 5
               </option>
-            </select>
-            <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+            </select> */}
+            <label
+              htmlFor=""
+              className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
               Password
             </label>{" "}
             <br />
             <input
-              type="Password"
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              required
+              onChange={(e) => setPassword(e.target.value)}
               className="bg-[#f4f4f4] w-full lg:h-[65px] h-12 mb-4 lg:mb-[2.688rem] rounded-[6px] placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
             />
-            <label htmlFor="" className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
+            <label
+              htmlFor=""
+              className="text-[#333333] lg:text-base text-sm font-normal leading-6 break-words">
               Confirm Password
             </label>{" "}
             <br />
             <input
               type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               className="bg-[#f4f4f4] w-full lg:h-[65px] mb-4 h-12 rounded-[6px] placeholder-[#666666] font-normal lg:text-base text-sm lg:ps-[2.688rem] ps-4"
             />
             <div className="flex justify-center">
               <button className=" py-[0.646rem] px-[1.375rem] rounded-[5px] bg-[#FF8800] text-white text-base font-semibold break-words mx-auto">
-                Continue
+        {isPending ? (
+          <Loader className="animate-spin h-4 w-4" />
+        ) : (
+          "Continue"
+        )}
               </button>
             </div>
           </form>
---
+++ types/interface.ts
@@ -0,0 +1,10 @@
+{
+    "data": {
+        "name": "olawale funmilayo",
+        "email": "folawale119@gmail.co",
+        "date_of_birth": "2006-10-23T09:12:09.000000Z",
+        "phone_number": "08067522171"
+    },
+    "status": true,
+    "message": "User registered successfully. Please check your email to verify your account."
+}
