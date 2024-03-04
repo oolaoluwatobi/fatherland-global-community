f7c718ec7f3137cb6bb0e71642a5ba4b409c90fa
OlatunjiAbiodunEmmanuel <olatunjia889@gmail.com>
2024-03-04 13:36:06 +0100

    add payment ui

--- types/types.ts
+++ types/types.ts
@@ -1,36 +1,40 @@
-{
-    "data": {
-        "name": "olawale funmilayo",
-        "email": "folawale119@gmail.co",
-        "date_of_birth": "2006-10-23T09:12:09.000000Z",
-        "phone_number": "08067522171"
-    },
-    "status": true,
-    "message": "User registered successfully. Please check your email to verify your account."
-}
+interface RootObject {
+    data: Data;
+    status: boolean;
+    message: string;
+  }
+  
+  interface Data {
+    name: string;
+    email: string;
+    date_of_birth: string;
+    phone_number: string;
+  }
 
-{
-    "data": {
-        "id": 18,
-        "first_name": "Temitope",
-        "middle_name": null,
-        "last_name": "tope",
-        "email": "temitopeoyin8@gmail.com",
-        "email_verified_at": "2024-01-29T13:26:11.904748Z",
-        "date_of_birth": "2006-10-23",
-        "country": "Nigeria",
-        "city_state_province": "Oyo",
-        "phone_number": "09038349452",
-        "membership_id": null,
-        "profile_picture": null,
-        "interests": [ ],
-        "created_at": "2024-01-29 09:43:12",
-        "updated_at": "2024-01-29 13:26:11",
-        "deleted_at": null
-    },
-    "token": "3|nRMaLzSaKsmEZF1vCbpZZX6cw9I9k6SsFHg5XqDv3a95c6d7",
-    "message": "User verified successfully."
-}
+interface RootObject {
+    data: Data;
+    token: string;
+    message: string;
+  }
+  
+  interface Data {
+    id: number;
+    first_name: string;
+    middle_name?: any;
+    last_name: string;
+    email: string;
+    email_verified_at: string;
+    date_of_birth: string;
+    country: string;
+    city_state_province: string;
+    phone_number: string;
+    membership_id?: any;
+    profile_picture?: any;
+    interests: any[];
+    created_at: string;
+    updated_at: string;
+    deleted_at?: any;
+  }
 
 type User = {
     name: string;
