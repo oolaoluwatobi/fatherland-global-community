c3fcfd1c2be723da6e4caddfabbca8844aaeb311
Temitopeoyin <temitopeoyin8@gmail.com>
2024-03-11 16:12:09 +0100

    store

--- app/(community)/store/component/ProductCardHotDeals.tsx
+++ app/(community)/store/component/ProductCardHotDeals.tsx
@@ -37,18 +37,18 @@ export default function ProductCardHotDeals({
   discount,
 }: Props) {
   return (
-    <div className=" shadow pb-7 hover:border-secondary  active:border active:border-primary hover:border bg-white">
+    <div className=" shadow pb-5 hover:border-secondary  active:border active:border-primary hover:border bg-white">
       <div className="relative">
-        <Image src={image!!} alt="" className="w-full" />
+        <Image src={image} alt="" className="w-full bgred-500 h-[16.8rem] " />
         {discount ?  <p className="absolute top-6 left-0 bg-[#FE5104] text-white px-4 text-xs py-2  ">{discount}</p> : null}
       </div>
       <div className="ml-4">
-        <p className="font-[500] text-lg w-[18rem] pt-4">{content}</p>
+        <p className="font-[500] lg:text-lg  text-sm w-[18rem] pt-4">{content}</p>
         <p className="text-gray-400  lg:text-lg pt-1">{content1}</p>
         <p className="pt-1">{content2}</p>
-        <div className="lg:flex gap-2">
+        <div className="flex gap-2">
           <p className="">{price}</p>
-          <p className="text-orange-400">{price1}</p>
+        {price1 ?  <p className="text-orange-400">{price1}</p> : null } 
         </div>
       </div>
     </div>
